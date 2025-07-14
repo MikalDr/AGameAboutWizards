b75ac031e838be2566bbe315caffdc8f7c938537
MikalDr <mikal-dr@hotmail.com>
2025-07-14 22:30:15 +0200

    Accelerated movement and Sprite

--- player/PlayerMovement.cs
+++ player/PlayerMovement.cs
@@ -2,16 +2,47 @@ using Godot;
 
 public partial class PlayerMovement : CharacterBody2D
 {
+	[Export]
+	private float MaxSpeed = 400f;
+	[Export]
+	private float Acceleration = 3000f;
+
+	private AnimatedSprite2D _anim;
+
 	public override void _EnterTree()
 	{
 		SetMultiplayerAuthority(int.Parse(Name));
+	}
+
+	public override void _Ready()
+	{
+		_anim = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		_anim.Animation = "idle";
 	}
 
 	public override void _Process(double delta)
 	{
 		if (IsMultiplayerAuthority())
 		{
-			Velocity = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down") * 400;
+			Vector2 inputDirection = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+
+			if (inputDirection != Vector2.Zero)
+			{
+				inputDirection = inputDirection.Normalized();
+				Velocity = Velocity.MoveToward(inputDirection * MaxSpeed, Acceleration * (float)delta);
+
+				_anim.Animation = "run";
+
+				if (!_anim.IsPlaying())
+					_anim.Play();
+			}
+			else
+			{
+				Velocity = Vector2.Zero;
+				_anim.Stop();
+				_anim.Animation = "idle";
+			}
+
 		}
 		
 		MoveAndSlide();
